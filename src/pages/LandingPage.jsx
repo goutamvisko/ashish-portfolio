2e6227dcb033ab857edfb7fc6f2d4908c342e742
goutamvisko <goutam.visko@gmail.com>
2025-09-25 11:00:16 +0530

    pushed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,19 +6,19 @@ import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import Education from "../components/Education";
 import Experience from "../components/Experience";
-import Inquiry from "../components/Inquiry"
+// import Inquiry from "../components/Inquiry"
 import Contact from "../components/Contact" 
 export default function LandingPage() {
-    const [showInquiry, setShowInquiry] = useState(false);
+    // const [showInquiry, setShowInquiry] = useState(false);
 
     // Show inquiry form modal only on first visit
-  useEffect(() => {
-    const hasVisited = localStorage.getItem("isInquiry");
-    if (!hasVisited) {
-      setShowInquiry(true);
-      localStorage.setItem("isInquiry", "true");
-    }
-  }, []);
+  // useEffect(() => {
+  //   const hasVisited = localStorage.getItem("isInquiry");
+  //   if (!hasVisited) {
+  //     setShowInquiry(true);
+  //     localStorage.setItem("isInquiry", "true");
+  //   }
+  // }, []);
   
   return (
     <>
@@ -32,12 +32,12 @@ export default function LandingPage() {
       <Contact id="contact"/>
 
       {/*Inquiry Modal Popup */}
-      {showInquiry && (
+      {/* {showInquiry && (
         <Inquiry
           isOpen={showInquiry}
           onClose={() => setShowInquiry(false)}
         />
-      )}
+      )} */}
     </>
   );
 }
